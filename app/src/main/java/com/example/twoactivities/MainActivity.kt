399218a016e3db636c8f3354055193315fa581a4
Jefferson <jefferson.toledo@fengbrasil.com.br>
2021-12-03 22:56:53 -0300

    Task 1-v1

--- app/src/main/java/com/example/twoactivities/MainActivity.kt
+++ app/src/main/java/com/example/twoactivities/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.twoactivities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+//     val LOG_TAG = MainActivity.getSimpleName();
+    val clicked: String = "Valeu"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun launchSecondActivity(view: View) {
+//        Log.d(clicked,"Button Clicked!");
+        Log.d(clicked,"Button Clicked!");
     }
 }
