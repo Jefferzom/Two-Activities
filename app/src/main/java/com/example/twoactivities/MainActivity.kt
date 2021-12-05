eb58fd57f8fa233dfe4753e1ef9d232bef2f6080
Jefferson <jefferson.toledo@fengbrasil.com.br>
2021-12-05 20:27:32 -0300

    WIP: finishing task 4

--- app/src/main/java/com/example/twoactivities/MainActivity.kt
+++ app/src/main/java/com/example/twoactivities/MainActivity.kt
@@ -6,13 +6,18 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.EditText
+import android.widget.TextView
 
 const val EXTRA_MESSAGE = "com.example.twoactivities.MESSAGE";
+
+const val TEXT_REQUEST = 1;
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var mReplayHeadTextView = findViewById<TextView>(R.id.text_header_replay)
+        var mReplayTextView = findViewById<TextView>(R.id.text_message_replay)
     }
 
     fun launchSecondActivity(view: View) {
@@ -26,6 +31,6 @@ class MainActivity : AppCompatActivity() {
             }
             putExtra(EXTRA_MESSAGE, message)
         }
-        startActivity(intent)
+        onActivityResult(intent, TEXT_REQUEST)
     }
 }
--- app/src/main/java/com/example/twoactivities/SecondActivity.kt
+++ app/src/main/java/com/example/twoactivities/SecondActivity.kt
@@ -1,8 +1,13 @@
 package com.example.twoactivities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import android.widget.TextView
+
+const val EXTRA_REPLAY = "com.example.twoactivities.REPLAY";
 
 class SecondActivity : AppCompatActivity() {
 
@@ -14,6 +19,17 @@ class SecondActivity : AppCompatActivity() {
 
         val textView = findViewById<TextView>(R.id.text_header).apply {
             text = message
+        }
+
+        val mReplay = findViewById<EditText>(R.id.editText_second);
+
+        fun returnReplay(view: View) {
+            val replay: String = mReplay.text.toString();
+            val intent = Intent(this, MainActivity::class.java).apply {
+                putExtra(EXTRA_REPLAY, replay)
+            }
+            setResult(RESULT_OK, intent)
+            finish();
         }
     }
 }
